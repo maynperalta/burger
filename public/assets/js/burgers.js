5f627d12e169b3341c4c8106783831547eb93520
Maynard Peralta <maynperalta@gmail.com>
2020-09-19 20:29:28 -0600

    created new .js file

---
+++ public/assets/js/burgers.js
@@ -0,0 +1,42 @@
+$(function() {
+    $(".create-form").on("submit", function(event) {
+        event.preventDefault();
+
+        var newBurger ={
+            name: $("#newBurger").val().trim(),
+            devoured: 0
+        };
+
+        $.ajax("/api/burgers", {
+            type: "POST",
+            data: newBurger
+        }).then(
+            function() {
+                console.log("Created a new burger.");
+                location.reload();
+            });
+    });
+    $(".devour").on("click", function(event) {
+        event.preventDefault();
+        var id = $(this).data("id");
+        var devouredState ={
+            devoured: 1
+        };
+        $.ajax("/api/burgers" + id, {
+            type: "PUT",
+            data: devouredState
+        }).then(function() {
+            console.log("Burger devoured.");
+            location.reload();
+        });
+    });
+});
+
+$(".delete").on("click", function(event) {
+    event.preventDefault();
+    var id = $(this).data("id");
+    $.ajax({
+        type: "DELETE",
+        url: "/api/burgers" + id
+    }).then(location.reload());
+});
