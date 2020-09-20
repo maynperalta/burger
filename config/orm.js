47de827cb33eb2acd70d0c580ce421c938b45966
Maynard Peralta <maynperalta@gmail.com>
2020-09-19 22:20:42 -0600

    updated orm

--- config/orm.js
+++ config/orm.js
@@ -1,26 +1,58 @@
 const connection = require("./connection.js");
 
+function printQuestionMarks(num) {
+    var arr = [];
+    for(var i = 0; i < num; i++) {
+        arr.push("?");
+    }
+    return arr.toString();
+};
+
+function objToSql(ob) {
+    var arr = [];
+
+    for (var key in ob) {
+        var value = ob[key];
+        if (Object.hasOwnProperty.call(ob, key)) {
+            if(typeof value === "string" && value.indexOf(" ") >= 0) {
+                value = "'" + value + "'";
+            }
+            arr.push(key + "=" + value);
+        }
+    }
+    return arr.toString();
+}
+
 
 const DB = {
-    create: (tableName, columns, values, cb) => {
-        connection.query("INSERT into ?? (?, ?) VALUES (?, ?)", [tableName, ...columns, ...values], (err, data) => {
+    create: (table, cols, vals, cb) => {
+        var queryString = "INSERT INTO " + table + " (" + cols.toString() + ") " + "VALUES (" + printQuestionMarks(vals.length) + ") ";
+        connection.query(queryString, vals, function(err, result) {
             if(err) throw err;
-            cb(data);
-        })
-
+            cb(result);
+        });
     },
-    read: (tableName, cb) => {
-        connection.query("SELECT * FROM ??", [tableName], (err, data) => {
+    all: (tableInput, cb) => {
+        var queryString = "SELECT * FROM " + tableInput + ";";
+        connection.query(queryString, function(err, result) {
             if(err) throw err;
-            cb(data);
-        })
+            cb(result);
+        });
     },
-    update: () => {
-
+    update: (table, objColVals, condition, cb) => {
+        var queryString = "UPDATE " + table + "SET" + objToSql(objColVals) + " WHERE " + condition;
+        connection.queryString(queryString, function(err, result) {
+            if(err) throw err;
+            cb(result);
+        });
     },
-    delete: () => {
-
+    delete: (table, condition, cb) => {
+        var queryString = "DELETE FROM " + table + "WHERE" + condition;
+        connection.query(queryString, function(err, result) {
+            if(err) throw err;
+            cb(result);
+        });
     }
-}
+};
 
 module.exports = DB;
